4e27f7c7aaefc44835ae297d166c9c4538806131
Mihai Racoreanu <Mihai.Racoreanu@dvla.gov.uk>
2023-05-10 19:54:43 +0100

    Initial commit, added some application.properties

---
+++ src/main/java/com/roadtaxapi/roadtax/RoadTaxApplication.java
@@ -0,0 +1,14 @@
+package com.roadtaxapi.roadtax;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RoadTaxApplication
+{
+
+    public static void main(String[] args) {
+        SpringApplication.run(RoadTaxApplication.class, args);
+    }
+
+}
